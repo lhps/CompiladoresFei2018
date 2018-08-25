c85b77839fb030c89953bf850885431dda190237
lucaspin <lucashpsfei@gmail.com>
2018-08-25 16:01:43 -0300

    Projeto

---
+++ Projeto1/Projeto1/b.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Variáveis globais
+char palavra[100];
+int pos = 0;
+
+//Protótipos das funções
+voidaceita();
+void rejeita();
+
+void aceita(){
+	printf("Aceita!");
+}
+
+void rejeita(){
+	printf("Rejeita!");
+}
+
+//Main
+int main(){
+	gets(palavra);
+	pos = 0;
+	e-842150451();
+}
+
